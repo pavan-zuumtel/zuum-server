1c2f4585478a3e437094838fac624701b24f6b7f
Pavan <pavan@pavans-imac.att.net>
2014-12-15 13:00:50 -0800

    corrected a syntax error in index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
-	response.writehead(200, {"Content-Type": "text/plain"});
+	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Just testing");
 	response.end();
 }).listen(5000);
