9269e8ff7d87add70a87ea853258ee5d4262231c
Pavan <pavan@pavans-imac.att.net>
2014-12-15 16:20:27 -0800

    initial test bed for POST data

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+//app.use(express.bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.get('/', function(request, response) {
-	response.send("Just Testing");
+	// response.send("Just Testing");
+	response.send(fs.readFileSync('index.html').toString());
+});
+
+app.post('/', function(request, response) {
+	response.send("" +request.body.tag +"<br>"+request.body.time);
+	//response.send(request.body.time);
 });
 
 app.listen(app.get('port'), function() {
