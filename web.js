fb9cf2c7de6899137d2568db7b74500d4e131369
Pavan <pavan@pavans-imac.att.net>
2014-12-16 18:15:25 -0800

    Unfinished work; currently at processing POST requests

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
-var fs = require('fs');
 var bodyParser = require('body-parser');
 var Firebase = require('firebase');
 
 var app = express();
 var myFirebaseRef = new Firebase("https://flickering-heat-3988.firebaseio.com/");
+var carId;
+
+myFirebaseRef.remove();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -16,25 +18,39 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', function(request, response) {
 	// response.send("Just Testing");
-	response.send(fs.readFileSync('index.html').toString());
+	response.sendFile('index.html', { root: __dirname });
 });
 
 app.post('/', function(request, response) {
-	response.send("" +request.body.reader_name +"<br>"+request.body.mac_address);
-	myFirebasRef.set({
-		reader_name: request.body.reader_name,
-		mac_address: request.body.mac_address,
-		field_names: request.body.field_names,
-		field_values: request.body.field_values
-	});
+	// TODO: Check the field_values 
 
+	/*carId = myFirebaseRef.child(request.body.mac_address);
+	carId.set({
+		// reader_name: request.body.reader_name,
+		// mac_address: request.body.mac_address,
+		'field_names': request.body.field_names,
+		'field_values': request.body.field_values
+	});*/
+
+	console.log("New One\n");
 	console.log(request.body.reader_name);
 	console.log(request.body.mac_address);
 	console.log(request.body.line_ending);
 	console.log(request.body.field_delim);
 	console.log(request.body.field_names);
 	console.log(request.body.field_values);
+	request.body.field_values = []
 	//response.send(request.body.time);
+});
+
+app.post('/fromManheim', function(request, response) {
+	
+	response.send("" +request.body.tag +"<br>"+request.body.time);
+	myFirebaseRef.on("value", function(snapshot) {
+		console.log(snapshot.val());
+	}, function(errorObject) {
+		console.log("Read operation failed: " +errorObject.code);
+	});
 });
 
 app.listen(app.get('port'), function() {
