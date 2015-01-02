f57b2166b59652f1f870b3949d4a8e414e88ee12
Pavan <pavan@pavans-imac.att.net>
2015-01-02 10:46:42 -0800

    Proper Indentation

--- apis/edmunds/edmunds.js
+++ apis/edmunds/edmunds.js
@@ -6,39 +6,39 @@ var api_key = 'nj3gudefmd9kfatezgheaz7b';
 var endpoint = '/api/vehicle/v2/vins';
 
 var options = {
-	hostname: HOST 
+  hostname: HOST 
 };
 
 var data = '';
 var error = '';
 function decodeVin(){
-	http.request(options, function(response) {
-		response.on('data', function(chunk) {
-			data += chunk;
-		});
-		response.on('end', function() {
-			console.log(data);
-			console.log("I'm jere");
-			return {
-				data: data,
-				error: error
-			}
-		});
-		if(response.statusCode != 200) {
-			error = true;
-		} else {
-			error = false;
-		}
-	}).end();
+  http.request(options, function(response) {
+    response.on('data', function(chunk) {
+            data += chunk;
+    });
+    response.on('end', function() {
+            console.log(data);
+            console.log("I'm jere");
+            return {
+                    data: data,
+                    error: error
+            }
+    });
+    if(response.statusCode != 200) {
+            error = true;
+    } else {
+            error = false;
+    }
+  }).end();
 }
 
 function getInfo(vin_number) {
-	// construct the path to make the api call
-	var path = endpoint + vin_number + '?fmt=' + fmt + '&api_key=' + api_key;
-	options.path = path;
+  // construct the path to make the api call
+  var path = endpoint + vin_number + '?fmt=' + fmt + '&api_key=' + api_key;
+  options.path = path;
 
-	console.log(decodeVin());
-	console.log('Now..');
+  console.log(decodeVin());
+  console.log('Now..');
 }
 
 exports.getInfo = getInfo;
--- web.js
+++ web.js
@@ -8,25 +8,25 @@ var myFirebaseRef = new Firebase("https://flickering-heat-3988.firebaseio.com/")
 var auctionSite = "";	// Associate the mac_address of the reader at a place
 var readerId;	// stores the ref to the reader/auctions location
 
-var allReaders = []	// list of all readers cuurrently in auction
+var allReaders = [];	// list of all readers cuurrently in auction
 /* 
-   Configure smtp server details
+Configure smtp server details
 */
 var transport = nodemailer.createTransport( {
-	service: "Gmail",
-	auth: {
-		user: "zuum.email@gmail.com",
-		pass: "zuum.tel"
-	}
+  service: "Gmail",
+  auth: {
+    user: "zuum.email@gmail.com",
+    pass: "zuum.tel"
+  }
 });
 
 // TODO: Include the gateway addresses of all known mobile carriers if
 // using thid method to send SMS
 carrierSMTPFormat = {
-	"AT&T": "@txt.att.net",
-	"Sprint": "@pm.sprint.com",
-	"T-Mobile": "@tmomail.net",
-	"Verizon": "@vtext.com" 
+  "AT&T": "@txt.att.net",
+  "Sprint": "@pm.sprint.com",
+  "T-Mobile": "@tmomail.net",
+  "Verizon": "@vtext.com" 
 };
 
 myFirebaseRef.remove();
@@ -36,108 +36,105 @@ app.use(express.static(__dirname + '/public'));
 //app.use(express.bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 
 app.get('/', function(request, response) {
-	// response.send("Just Testing");
-	response.sendFile('index.html', { root: __dirname });
+  // response.send("Just Testing");
+  response.sendFile('index.html', { root: __dirname });
 });
 
 app.post('/', function(request, response) {
-	
-	// store the required POST data in an array where each cell corresponds
-	// to the info of a particular tag/car. 
-	// response.send(request.body.field_values);
-	cars_info = request.body.field_values.split("\n");
-	cars_info.pop();	// last element is an empty string
-	auctionSite = request.body.mac_address.split('"').join("");
-	if (allReaders.indexOf(auctionSite) == -1) {
-		// If this is the first time, a reader is sending data, it
-		// probably means the auction has started. So delete all the
-		// data sent by this reader after 8 hrs.
-		readerId = myFirebaseRef.child(auctionSite);
-		setTimeout(clearData, 8*60*60*1000, auctionSite);
+  
+  // store the required POST data in an array where each cell corresponds
+  // to the info of a particular tag/car. 
+  // response.send(request.body.field_values);
+  cars_info = request.body.field_values.split("\n");
+  cars_info.pop();	// last element is an empty string
+  auctionSite = request.body.mac_address.split('"').join("");
+  if (allReaders.indexOf(auctionSite) == -1) {
+    // If this is the first time, a reader is sending data, it
+    // probably means the auction has started. So delete all the
+    // data sent by this reader after 8 hrs.
+    readerId = myFirebaseRef.child(auctionSite);
+    setTimeout(clearData, 8*60*60*1000, auctionSite);
 
-		allReaders.push(auctionSite); 
-	}
+    allReaders.push(auctionSite); 
+  }
 
-	var antenna_id = 0;
-	var epc = 1;
-	var first_seen_time = 2;
-	var RSSI = 3;
-	for (eachCar in cars_info) {
-		// eachCar will be in the form of"antenna_id,"epc",ts,RSSI"
-		carInfo = cars_info[eachCar].split(",");
-		carID = carInfo[epc].split('"').join("");
+  var antenna_id = 0;
+  var epc = 1;
+  var first_seen_time = 2;
+  var RSSI = 3;
+  for (var eachCar in cars_info) {
+    // eachCar will be in the form of"antenna_id,"epc",ts,RSSI"
+    carInfo = cars_info[eachCar].split(",");
+    carID = carInfo[epc].split('"').join("");
 
-		readerId.child(carID).set({
-			
-			'Antennaid': carInfo[antenna_id],
-			// divide the ts received by 1000 in order to work with Date() (Not sure if this is the best way to do but for now ...)
-			'First_seen_time': (new Date(parseInt(carInfo[first_seen_time])/1000)).toLocaleString(),
-			'RSSI': carInfo[RSSI]
-		});
+    readerId.child(carID).set({
+      'Antennaid': carInfo[antenna_id],
+      // divide the ts received by 1000 in order to work with Date() (Not sure if this is the best way to do but for now ...)
+      'First_seen_time': (new Date(parseInt(carInfo[first_seen_time])/1000)).toLocaleString(),
+      'RSSI': carInfo[RSSI]
+    });
+  }
 
-	}
-
-	response.end();
+  response.end();
 
 });
 
 function clearData(auctionSite) {
-	ref = new Firebase(myFirebaseRef + '/' + auctionSite);
-	ref.remove();
+  ref = new Firebase(myFirebaseRef + '/' + auctionSite);
+  ref.remove();
 
-	ind = allReaders.indexOf(auctionSite);
-	allReaders.splice(ind, 1);
+  ind = allReaders.indexOf(auctionSite);
+  allReaders.splice(ind, 1);
 }
 
 app.post('/fromManheim', function(request, response) {
-	
-	var tagID = request.body.tag_id;
-	var mobileNumber = request.body.mobile_number;
-	var carrier = request.body.carrier_name;
+  
+  var tagID = request.body.tag_id;
+  var mobileNumber = request.body.mobile_number;
+  var carrier = request.body.carrier_name;
 
-	if (mobileNumber.trim().length != 10) {
-	// Only checks the length of the number but not whether it contains chars or numbers 
-	response.end("Not a valid number");
+  if (mobileNumber.trim().length != 10) {
+    // Only checks the length of the number but not whether it contains chars or numbers 
+    response.end("Not a valid number");
 }
 
-	if (auctionSite == "") {
-		response.send("Auction has not yet started. So your request will not be considered");
-		response.end();
-	}
-	tagRef = myFirebaseRef.child(auctionSite).child(tagID);
-	tagRef.on("value", sendSMS);
+  if (auctionSite === "") {
+    response.send("Auction has not yet started. So your request will not be considered");
+    response.end();
+  }
+  tagRef = myFirebaseRef.child(auctionSite).child(tagID);
+  tagRef.on("value", sendSMS);
 
-	function sendSMS(snapshot) {
-		var carInfo = snapshot.val();
-		if (carInfo != null) {
-			// send SMS and detach callback
-
-			var mailOptions = {
-				from: "zuum.email@gmail.com",
-				to: mobileNumber.trim() + carrierSMTPFormat[carrier].trim(),
-				subject: "Testing ....",
-				text: "car was at " + carInfo.First_seen_time
-			}
-			transport.sendMail(mailOptions, function(error, response) {
-				if (error) {
-					console.log(error);
-				} else {
-					console.log("Message sent");
-				}
-			});
-			console.log("SMS ...", carInfo);
-			// detach the callback after sending SMS
-			tagRef.off("value", sendSMS);
-			console.log("After Callback ", tagID);
-		}
-	}
-	response.end();
+  function sendSMS(snapshot) {
+    var carInfo = snapshot.val();
+    if (carInfo !== null) {
+      // send SMS and detach callback
+      var mailOptions = {
+        from: "zuum.email@gmail.com",
+        to: mobileNumber.trim() + carrierSMTPFormat[carrier].trim(),
+        subject: "Testing ....",
+        text: "car was at " + carInfo.First_seen_time
+      };
+      transport.sendMail(mailOptions, function(error, response) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Message sent");
+        }
+      });
+      console.log("SMS ...", carInfo);
+      // detach the callback after sending SMS
+      tagRef.off("value", sendSMS);
+      console.log("After Callback ", tagID);
+    }
+  }
+  response.end();
 });
 
 app.listen(app.get('port'), function() {
-	console.log("Node app is running at localhost:" +app.get('port'));
+  console.log("Node app is running at localhost:" +app.get('port'));
 });
